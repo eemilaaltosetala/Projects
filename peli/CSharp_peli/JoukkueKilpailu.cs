da0891d799645245e011af8bab237cd3fff66140
eemilaaltosetala <57128549+eemilaaltosetala@users.noreply.github.com>
2022-02-04 16:03:58 +0200

    Update JoukkueKilpailu.cs

--- peli/CSharp_peli/JoukkueKilpailu.cs
+++ peli/CSharp_peli/JoukkueKilpailu.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Tentti03
+namespace peli
 {
     //Luokka Joukkuekilpailu, joka on geneerinen, Kilpailu-luokan aliluokka
     class JoukkueKilpailu<Joukkue, TTulos> : Kilpailu<Joukkue, int>
