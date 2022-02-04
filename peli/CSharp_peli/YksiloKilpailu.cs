f381b274cade52c6f7d632368cf15778833fb297
eemilaaltosetala <57128549+eemilaaltosetala@users.noreply.github.com>
2022-02-04 16:05:54 +0200

    Update YksiloKilpailu.cs

--- peli/CSharp_peli/YksiloKilpailu.cs
+++ peli/CSharp_peli/YksiloKilpailu.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Tentti03
+namespace peli
 {
     //Luokka YksiloKilpailu, geneerinen, Kilpailu-luokan aliluokka
     class YksiloKilpailu<Henkilo, TTulos> : Kilpailu<Henkilo, double>
